6742cf64416f1f7c1390f2b7da8f7373b1e7004e
Krisjanis Veinbahs <krisjanisv@scandiweb.com>
2018-07-21 19:27:16 +0300

    Ruby interpolation syntax mistake fix

--- deploy/chef/app-cookbook/recipes/persist.rb
+++ deploy/chef/app-cookbook/recipes/persist.rb
@@ -1,13 +1,7 @@
-persistant_volume_prefix = ENV['PROJECT_CODE'] ? ENV['PROJECT_CODE'] + '-' : ''
-persistant_volume_device = '/dev/disk/by-id/scsi-0DO_Volume_#{persistance_volume_prefix}persist'
-persistant_volume_mount  = '/mnt/persist'
-
-puts "Persistant volume device - '#{persistant_volume_device}'"
-puts "Persistant volume mount - '#{persistant_volume_mount}'"
-
 filesystem 'persist' do
+  persistant_volume_prefix = ENV['PROJECT_CODE'] ? ENV['PROJECT_CODE'] + '-' : ''
   fstype 'ext4'
-  device persistant_volume_device
-  mount persistant_volume_mount
+  device "/dev/disk/by-id/scsi-0DO_Volume_#{persistant_volume_prefix}persist"
+  mount "/mnt/persist"
   action [:create, :enable, :mount]
 end
